6b2ef39b61991ab77750f02ccaf5da543bf7fdfc
paulaji <paulajiparayil123@gmail.com>
2023-04-24 13:18:40 +0530

    added div for Q&A

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
 import "./app.css";
+import { useState } from "react";
+import Trivia from "./components/Trivia.jsx";
 
 function App() {
+  const [questionNumber, setQuestionNumber] = useState(5);
+  const moneyPyramid = [
+    { id: 1, amount: "2 Coconuts" },
+    { id: 2, amount: "1kg Banana (Poovan)" },
+    { id: 3, amount: "25kg Matta Rice Sack" },
+    { id: 4, amount: "20 Packet Charapara Easy Paalappam Mix" },
+    { id: 5, amount: "Vestige Induction Cooktop" },
+    { id: 6, amount: "Thankam Gold's gift voucher" },
+    { id: 7, amount: "Lalism (Sri. Mohananlal Ganamela) ticket" },
+    { id: 8, amount: "Date with BaChe (Baby Chemmanur)" },
+    { id: 9, amount: "KulirThennal 5star rated AC" },
+    { id: 10, amount: "1000 OTTA brand Premium Shirt" },
+    { id: 11, amount: "100 BSA LadyBoy cycles" },
+    { id: 12, amount: "50 iKall 14 Pro (2MP front + back)" },
+    { id: 13, amount: "Joe Alukkas Diamond Necklace" },
+    {
+      id: 14,
+      amount:
+        "Trip with Dr. Bobin Radhakrishnan (7day trip, no-shouting package)",
+    },
+    { id: 15, amount: "Skyline 12B flat" },
+  ].reverse();
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+          <Trivia />
+        </div>
+      </div>
+
       <div className="pyramid">
         <ul className="moneyList">
-          <li className="moneyListItem active">
-            <span className="moneyListItemNumber">1</span>
-            <span className="moneyListItemAmount">2 Coconuts</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">2</span>
-            <span className="moneyListItemAmount">1kg Poovan Banana</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">3</span>
-            <span className="moneyListItemAmount">25kg Matta Rice Sack</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">4</span>
-            <span className="moneyListItemAmount">Nolta Kitchen Set</span>
-          </li>
+          {moneyPyramid.map((m) => (
+            <li
+              className={
+                questionNumber === m.id
+                  ? "moneyListItem active"
+                  : "moneyListItem"
+              }
+            >
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">How are you?</div>
+      <div className="answers">
+        <div className="answer">fine</div>
+        <div className="answer">5n</div>
+        <div className="answer">FINE</div>
+        <div className="answer">Fine! Thank You</div>
+      </div>
+    </div>
+  );
+}
